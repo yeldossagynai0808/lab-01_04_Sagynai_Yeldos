b6e9c81af5667324a5d6a5d5ca62eb0217b46e9f
yeldossagynai0808 <yeldos.sagynai@narxoz.kz>
2026-04-11 09:40:54 +0500

    practise lab-04

--- lab-04/practise.py
+++ lab-04/practise.py
@@ -1,10 +1,18 @@
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Сервер работает"}
+# @app.get("/")
+# def home():
+#     return {"message": "Сервер работает"}
 
 #uvicorn practise:app --reload
+
+class Mage(Player):
+    def event(self, event):
+        if event.type == "LOOK":
+            item = item.data["Item"]
+            item.damage *= 1.1
+            return f"(Mage damage = {item.damage})"
+            
--- lab-04/task1-20.py
+++ lab-04/task1-20.py
@@ -8,97 +8,82 @@ app = FastAPI()
 
 
 ### 1 ###
-# class Player:
-#     def __init__(self, id, name, hp):
-#         self._id = id
-#         self._name = name.strip().title()
-#         self._hp = (lambda hp: hp if hp > 0 else 0)(hp)
+class  Player:
+    def __init__(self,id, name, hp):
+        self._id = id
+        self._name = name.strip().title()
+        if hp < 0:
+            self._hp = 0
+        else:
+            self._hp = hp
+
+    def __str__(self):
+        return f"Player({self._id}, name = {self._name}, hp = {self._hp})"
+
+    def __del__(self):
+        print("Player ({self._name}) удален")
+
+p = Player(1, "Yeldos", 100)
 
 
-        
-
-#     def __str__(self):
-#         return f'Player({self._id}, name = {self._name}, hp = {self._hp})'
-
-#     def __del__(self):
-#         print(f"Player {self._name} удален")
-
-# p = Player(1, "john", 120)
-# print(p)
-
-# @app.get('/')
-# def home():
-#     return str(p)
-
-# if __name__ == ("__main__"):
-#     app.run(debug=True)
 
 ### 2 ###
-# class Player:
-#     def __init__(self, id, name, hp):
-#         self.name = name.strip().title()
-#         self.hp = hp
-#         self.id = id
+class Player:
+    def __init__(self, id, name, hp):
+        self.id = id
+        self.name = name.strip().title()
+        self.hp = hp if hp > 0 else 0
 
-#     def __str__(self):
-#         return f"Player id = {self.id}, name = {self.name}, hp = {self.hp}"
-        
-#     @classmethod
-#     def from_string(cls, data: str):
-#         parts = data.split(",")
+    def __str__(self):
+        return f"Player(id={self.id}, name='{self.name}', hp={self.hp})"
 
-#         if len(parts) != 3:
-#             raise ValueError("Неверный формат")
+    @classmethod
+    def from_string(cls, data: str):
+        parts = data.split(",")
 
-#         id = int(parts[0].strip())
-#         name = parts[1].strip()
-#         hp = int(parts[2].strip())
+        if len(parts) != 3:
+            raise ValueError("Неверный формат строки")
 
-#         return cls(id, name, hp)
+        try:
+            id = int(parts[0].strip())
+            name = parts[1].strip()
+            hp = int(parts[2].strip())
+        except:
+            raise ValueError("Ошибка преобразования данных")
 
-# @app.get("/")
-# def home():
-#     return str(p)
+        return cls(id, name, hp)
+                
 
-# p = Player.from_string("2, alice, 90")
-# print(p)
+
+
 
 ### 3 ###
-# class Item:
-#     def __init__(self, id, name, power):
-#         self.id = id
-#         self.name = name.strip().title()
-#         self.power = power
+class Item:
+    def __init__(self, id, name, power):
+        self.id = id
+        self.name = name.strip().title()
+        self.power = power
 
-#     def __eq__(self, other):
-#         if not isinstance(other, Item):
-#             return False
-#         return self.id == other.id
+    def __hash__(self):
+        return self.hash()
 
-#     def __hash__(self):
-#         return hash(self.id)
+    def __eq__(self):
+        return 
 
-#     def __str__(self):
-#         return f"Item(id={self.id}, name='{self.name}', power ={self.power})"
+    def __str__(self):
+        return f"Item(id={self.id}, name = {self.name}, power={self.power})"
 
-# a = Item(1, "Sword", 50)
-# b = Item(1, "Gun", 100)
+i = Item(1, "Sword", 50)
+print(i)
 
-# print(a == b)
 
-# s = set()
-# s.add(a)
-# s.add(b)
 
-# print(len(s))
 
-# @app.get('/')
-# def home():
-#     return str(a==b)
 
-# @app.get('/s')
-# def answer():
-#     return str(len(s)) 
+
+
+
+
 
 
 ### 4 ###
@@ -170,14 +155,14 @@ app = FastAPI()
 # print(inv.get_strong_items(50))
 
 ### 6 ###
-class Event:
-    def __init__(self, type, data):
-        self.type = type
-        self.data = data
-        self.timestamp = datetime.now()
+# class Event:
+#     def __init__(self, type, data):
+#         self.type = type
+#         self.data = data
+#         self.timestamp = datetime.now()
 
-    def __str__(self):
-        return f"Event(type='{self.type}', data={self.data}, timestamp='{self.timestamp}')"
+#     def __str__(self):
+#         return f"Event(type='{self.type}', data={self.data}, timestamp='{self.timestamp}')"
 
 # e = Event("ATTACK", {"damage": 20})
 # print(e)
@@ -349,175 +334,175 @@ class Event:
 
 
 ### 11 ###
-def damage_stream(events):
-    for event in events:
-        if event.type == "ATTACK":
-            yield event.data["damage"]
+# def damage_stream(events):
+#     for event in events:
+#         if event.type == "ATTACK":
+#             yield event.data["damage"]
 
 ### 12 ###
-def generate_events(players, items, n):
-    events = []
+# def generate_events(players, items, n):
+#     events = []
 
-    event_type = lambda: random.choice(["ATTACK", "HEAL", "LOOT"])
+#     event_type = lambda: random.choice(["ATTACK", "HEAL", "LOOT"])
 
-    for player in players:
-        for _ in range(n):
-            t = event_type()
+#     for player in players:
+#         for _ in range(n):
+#             t = event_type()
 
-            if t == "ATTACK":
-                data = {"damage": random.randint(10, 100)}
+#             if t == "ATTACK":
+#                 data = {"damage": random.randint(10, 100)}
 
-            elif t == "HEAL":
-                data = {"heal": random.randint(5, 50)}
+#             elif t == "HEAL":
+#                 data = {"heal": random.randint(5, 50)}
 
-            else:
-                item = random.choice(items)
-                data = {"item": item}
+#             else:
+#                 item = random.choice(items)
+#                 data = {"item": item}
 
-            event = Event(t, data)
-            events.append(event)
+#             event = Event(t, data)
+#             events.append(event)
 
-    return events
+#     return events
 
-### 13 ###
-def analyze_logs(events):
-    total_damage = sum(
-        event.data["damage"]
-        for event in events
-        if event.type == "ATTACK"
-    )
+# ### 13 ###
+# def analyze_logs(events):
+#     total_damage = sum(
+#         event.data["damage"]
+#         for event in events
+#         if event.type == "ATTACK"
+#     )
 
-    damage_by_player = {}
-    event_count = {}
+#     damage_by_player = {}
+#     event_count = {}
 
-    for event in events:
+#     for event in events:
         
-        event_count[event.type] = event_count.get(event.type, 0) + 1
+#         event_count[event.type] = event_count.get(event.type, 0) + 1
 
     
-        if event.type == "ATTACK":
-            pid = event.player_id
-            damage = event.data["damage"]
-            damage_by_player[pid] = damage_by_player.get(pid, 0) + damage
+#         if event.type == "ATTACK":
+#             pid = event.player_id
+#             damage = event.data["damage"]
+#             damage_by_player[pid] = damage_by_player.get(pid, 0) + damage
 
-    top_player = max(damage_by_player, key=damage_by_player.get) if damage_by_player else None
-
-
-    most_common_event = max(event_count, key=event_count.get)
-
-    return {
-        "total_damage": total_damage,
-        "top_player": top_player,
-        "most_common_event": most_common_event
-    }
-
-### 14 ###
-decide_action = lambda player: (
-    "HEAL" if player.hp < 30
-    else "LOOT" if len(player.inventory) == 0
-    else "ATTACK"
-)
-
-# p1 = Player(1, 20)
-# p2 = Player(2, 100)
-
-# p2.inventory.append(Item(1, "Sword", 50))
-
-# print(decide_action(p1))  # HEAL
-# print(decide_action(p2)) 
+#     top_player = max(damage_by_player, key=damage_by_player.get) if damage_by_player else None
 
 
-### 16 ###
-class Player:
-    def __init__(self, id, hp):
-        self.id = id
-        self._hp = hp
-        self._inventory = []
+#     most_common_event = max(event_count, key=event_count.get)
 
-    
-    @property
-    def hp(self):
-        return self._hp
+#     return {
+#         "total_damage": total_damage,
+#         "top_player": top_player,
+#         "most_common_event": most_common_event
+#     }
 
-    @hp.setter
-    def hp(self, value):
-        if value < 0:
-            self._hp = 0
-        else:
-            self._hp = value
+# ### 14 ###
+# decide_action = lambda player: (
+#     "HEAL" if player.hp < 30
+#     else "LOOT" if len(player.inventory) == 0
+#     else "ATTACK"
+# )
+
+# # p1 = Player(1, 20)
+# # p2 = Player(2, 100)
+
+# # p2.inventory.append(Item(1, "Sword", 50))
+
+# # print(decide_action(p1))  # HEAL
+# # print(decide_action(p2)) 
+
+
+# ### 16 ###
+# class Player:
+#     def __init__(self, id, hp):
+#         self.id = id
+#         self._hp = hp
+#         self._inventory = []
 
     
-    @property
-    def inventory(self):
-        return self._inventory
+#     @property
+#     def hp(self):
+#         return self._hp
+
+#     @hp.setter
+#     def hp(self, value):
+#         if value < 0:
+#             self._hp = 0
+#         else:
+#             self._hp = value
 
     
-    def add_item(self, item):
-        self._inventory.append(item)
+#     @property
+#     def inventory(self):
+#         return self._inventory
 
-    def remove_item(self, item_id):
-        self._inventory = [i for i in self._inventory if i.id != item_id]
+    
+#     def add_item(self, item):
+#         self._inventory.append(item)
 
-    def __str__(self):
-        return f"Player(id={self.id}, hp={self._hp}, inventory={self._inventory})"
+#     def remove_item(self, item_id):
+#         self._inventory = [i for i in self._inventory if i.id != item_id]
 
-
-
-### 17 ###
-class Player:
-    def __init__(self, id, name, hp):
-        self.id = id
-        self.name = name
-        self._hp = hp
-        self._inventory = []
-
-    def __del__(self):
-        print(f"Player {self.name} удалён")
+#     def __str__(self):
+#         return f"Player(id={self.id}, hp={self._hp}, inventory={self._inventory})"
 
 
-### 18 ###
-class Inventory:
-    def __init__(self):
-        self._items = []
 
-    def add_item(self, item):
-        self._items.append(item)
+# ### 17 ###
+# class Player:
+#     def __init__(self, id, name, hp):
+#         self.id = id
+#         self.name = name
+#         self._hp = hp
+#         self._inventory = []
 
-    def __iter__(self):
-        return iter(self._items)
+#     def __del__(self):
+#         print(f"Player {self.name} удалён")
 
-# inv = Inventory()
 
-# inv.add_item(Item(1, "Sword", 50))
-# inv.add_item(Item(2, "Gun", 100))
-# inv.add_item(Item(3, "Knife", 20))
+# ### 18 ###
+# class Inventory:
+#     def __init__(self):
+#         self._items = []
 
-for item in inv:
-    print(item)
+#     def add_item(self, item):
+#         self._items.append(item)
 
-### 19 ### 
-def analyze_inventory(inventories):
-    all_items = [item for inv in inventories for item in inv]
+#     def __iter__(self):
+#         return iter(self._items)
 
-    unique_items = set(all_items)
+# # inv = Inventory()
 
-    top_item = max(all_items, key=lambda x: x.power) if all_items else None
+# # inv.add_item(Item(1, "Sword", 50))
+# # inv.add_item(Item(2, "Gun", 100))
+# # inv.add_item(Item(3, "Knife", 20))
 
-    return {
-        "unique_items": unique_items,
-        "top_power": top_item
-    }
-# inv1 = Inventory()
-# inv1.add_item(Item(1, "Sword", 50))
-# inv1.add_item(Item(2, "Gun", 100))
+# # for item in inv:
+# #     print(item)
 
-# inv2 = Inventory()
-# inv2.add_item(Item(1, "Sword", 50))
-# inv2.add_item(Item(3, "Knife", 20))
+# ### 19 ### 
+# def analyze_inventory(inventories):
+#     all_items = [item for inv in inventories for item in inv]
 
-# result = analyze_inventory([inv1, inv2])
+#     unique_items = set(all_items)
 
-# print(result)
+#     top_item = max(all_items, key=lambda x: x.power) if all_items else None
+
+#     return {
+#         "unique_items": unique_items,
+#         "top_power": top_item
+#     }
+# # inv1 = Inventory()
+# # inv1.add_item(Item(1, "Sword", 50))
+# # inv1.add_item(Item(2, "Gun", 100))
+
+# # inv2 = Inventory()
+# # inv2.add_item(Item(1, "Sword", 50))
+# # inv2.add_item(Item(3, "Knife", 20))
+
+# # result = analyze_inventory([inv1, inv2])
+
+# # print(result)
 
 
 
